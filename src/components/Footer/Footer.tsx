5e7c5d2a8b99b94359ef2486f18d5cd1ec62e65d
EDusik <eduardodusik@gmail.com>
2022-04-10 10:36:58 -0300

    feat(refactor): update Footer props

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import { HeroProps } from 'types'
+
+export function Footer({ hero }: HeroProps) {
+  return (
+    <div className="flex h-12	pt-5 justify-center items-center text-center bg-slate-300 text-slate-900">
+      <p dangerouslySetInnerHTML={{ __html: hero.footer }} ></p>
+    </div>
+  )
+}
--- src/components/Footer/index.tsx
+++
@@ -1,9 +0,0 @@
-import { HeroProps } from 'types'
-
-export function Footer({ hero }: HeroProps) {
-  return (
-    <div className="flex h-16	justify-center items-center text-center bg-slate-300 text-slate-900">
-      <p>Made with ♡ by <strong>{hero.name}</strong></p>
-    </div>
-  )
-}
