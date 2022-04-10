640cb877709cbaebe8b44c2fed1667358f7c9dcf
EDusik <eduardodusik@gmail.com>
2022-04-10 11:03:46 -0300

    feat(refactor): update types export

---
+++ src/types/Types.ts
@@ -0,0 +1,34 @@
+export interface IHero {
+	name: string;
+	description: string;
+	github: string;
+	linkedin: string;
+	footer: string;
+}
+
+export interface HeroProps {
+	hero: IHero;
+}
+
+export interface HomeProps {
+	hero: IHero;
+	pictures: IPicture[];
+}
+
+export interface IPicture {
+	slug: string;
+	title: string;
+	description: string;
+	creation_date: Date;
+	thumbnail: string;
+	badge: string;
+}
+
+export interface PictureProps {
+	picture: IPicture;
+	show: boolean[];
+}
+
+export interface PictureList {
+	pictures: IPicture[];
+}
